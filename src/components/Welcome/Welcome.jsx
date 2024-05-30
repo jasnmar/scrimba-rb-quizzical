3b3f146e14fb631ce3058b913ec6ddd3f5ef45c8
jasnmar <jasnmar@gmail.com>
2024-05-30 10:32:27 -0400

    Updating Description

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -4,7 +4,7 @@ function Welcome(props) {
     return (
         <main className="welcome--main">
             <h1>Quizzical</h1>
-            <h2>Some description if needed</h2>
+            <h2>A quick little quiz game</h2>
             <button onClick={props.clickHandler} className="btn">Start quiz</button>
         </main>
     )
